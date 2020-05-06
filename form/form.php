93de5ab8643dcf3928635ede4b5058bc2900bf2e
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2020-05-06 19:31:16 +0200

    Update form.php

--- form/form.php
+++ form/form.php
@@ -7,10 +7,6 @@
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
     <body>
-<?php
-include 'conexion.php';
-?>
-
 <div class="menu">
   <div class="text-center">
     <div class="btn-group" role="group" aria-label="...">
@@ -25,54 +21,27 @@ include 'conexion.php';
   
 <?php
         <div class="container">
-        <H1>Inserción de vivienda</H1><BR>
-            <form name="vivienda" action="proyecto.php" method="POST" enctype="multipart/form-data">
+        <H1>Datos de cliente</H1><BR>
+            <form name="vivienda" action="form.php" method="POST" enctype="multipart/form-data">
             <fieldset>
               <legend>Rellene el formulario</legend></br>
-                  <div >
-                  <div>
-                    <label for="tipo">*Tipo de vivienda:</label>
-                    <select id="tipo" name="tipo">
-                        <option selected="selected" value="selected">-seleccione-</option>
-<!-- Hacemos una consulta a todos los tipos en mysql -->                                                   
-                    </select>
- 
-             </div><br>
-
-            <div>
-                <label for="zona">*Zona:</label>
-                    <select id="zona" name="zona">
-                        <option selected="selected" value="selected">-seleccione-</option>                                               
-                    </select>
-            </div><br>
-         <br>
-
-            <div>
+                <div>
                 <label for="direccion">*Dirección:</label>
                     <input type="text" name="direccion" size="30" maxlength="50" placeholder="Introduzca la direccion">
-   
                 </div>            
-                <br><br>
-
-         
-                 <div>
+                <br>
+                <div>
                     <label for="precio">*Precio:</label>
-                    <input type="text"  
-                   name="precio" size="30" maxlength="12" placeholder="Introduzca el precio">€
+                    <input type="text" name="precio" size="30" maxlength="12" placeholder="Introduzca el precio">€
                    
-                </div><BR>
-
+                </div>
+                <br>
                 <div>
                     <label for="tamano">*Tamaño:</label>
-                    <input type="text" 
-                    name="tamano" size="30" maxlength="12" placeholder="Introduzca tamaño">metros cuadrados
-                    <BR><BR>
-                </div><BR>
-
-               
-
- 
-            <input type="submit" name="enviar" id="enviar" class="btn btn-default" value="Insertar vivienda">
+                    <input type="text" name="tamano" size="30" maxlength="12" placeholder="Introduzca tamaño">
+                </div>
+                <br>
+                <input type="submit" name="enviar" id="enviar" class="btn btn-default" value="Enviar">
             
             </div>
             </fieldset>
