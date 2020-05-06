93052a525817ec0e54f5105f61f8623872d149f4
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2020-05-06 21:02:32 +0200

    Update introduce.php

--- form/introduce.php
+++ form/introduce.php
@@ -5,15 +5,13 @@
   <body>
       <?php
         echo "Estoy aqui";
-        $servername="68.183.213.14:3306";
-        $username="usuario";
-        $password="1920";
-        $bd="sif_tfp";
+        $servername='68.183.213.14:3306';
+        $username='usuario';
+        $password='1920';
+        $bd='sif_tfp';
         // Creamos la conexión pasándole el servidor, nombre de user, y clave. Si no se establece conexión muestra un error.
-        $conexion = mysqli_connect( $servername, $username, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
-        //Seleccionamos la base de datos, que ya está declarada arriba.
-        $db = mysqli_select_db( $conexion, $bd ) or die ("No se ha podido conectar a la Base de datos");
-        
+        $conexion = mysqli_connect($servername, $username, $password, $bd) or die ("No se ha podido conectar al servidor de Base de datos");
+      
         echo "Conección a bd correcta";
         //Facilitamos recogida de datos con variables sencillas
         $ejercicio='2020';
