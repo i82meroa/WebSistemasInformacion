ddd2f8571bde506b75114c51236ab6cf739c4328
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2020-05-08 13:38:52 +0200

    Update introduce.php

--- form/introduce.php
+++ form/introduce.php
@@ -9,6 +9,7 @@
         $username="juanfran";
         $password="123";
         $bd="sif_tfp";
+        echo "Segundo echo OK";
         // Creamos la conexión pasándole el servidor, nombre de user, y clave. Si no se establece conexión muestra un error.
         $conexion = mysqli_connect($servername, $username, $password, $bd);
       
