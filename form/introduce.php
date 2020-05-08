e90489c2ee0a32cd06eefeb803997f35cdb75146
i82meroa <i82meroa@uco.es>
2020-05-08 16:36:36 +0200

    añadiendo la tabla variable con campo n_ventas

--- form/introduce.php
+++ form/introduce.php
@@ -4,10 +4,9 @@
   </head>
   <body>
       <?php
-        echo "Estoy aqui";
         // Creamos la conexión pasándole el servidor, nombre de user, y clave. Si no se establece conexión muestra un error.
         $conexion = mysqli_connect("68.183.213.14", "usuario", "1920", "sif_tfp","3306");
-        echo "Conección a bd correcta";
+        echo "Conección a bd correcta <br>";
      
       //Facilitamos recogida de datos con variables sencillas
         $ejercicio='2020';
@@ -16,17 +15,28 @@
         $Intereses=$_POST['Intereses'];
         $Despreciacion=$_POST['Despreciacion'];
         $Gastos_Generales=$_POST['Gastos_Generales'];
+        $Ventas=$_POST['Ventas'];
 
         //Insercion de datos
         $sql = "INSERT INTO DATOS_ACTIVOS (ANO, Beneficio_bruto, Impuestos, Intereses, Despreciacion, Gastos_Generales)
         VALUES ('$ejercicio', '$Beneficio_bruto', '$Impuestos', '$Intereses', '$Despreciacion', '$Gastos_Generales')";
         if (mysqli_query($conexion, $sql)) 
         {
-          echo "Registro añadido correctamente";
+          echo "Registro de 'Datos Activos' añadido correctamente <br>";
         } 
         else 
         {
-          echo "Error: No se pudieron introducir los datos" . mysqli_error($conexion);
+          echo "Error: No se pudieron introducir los datos de 'Datos activos' <br>" . mysqli_error($conexion);
+        }
+        $sql = "INSERT INTO VARIABLES (ANO, N_VENTAS)
+        VALUES ('$ejercicio', '$VENTAS')";
+        if (mysqli_query($conexion, $sql)) 
+        {
+          echo "Registro de 'VARIABLES' añadido correctamente <br>";
+        } 
+        else 
+        {
+          echo "Error: No se pudieron introducir los datos de 'VARIABLES' <br>" . mysqli_error($conexion);
         }
         mysqli_close($conexion);
       ?>
