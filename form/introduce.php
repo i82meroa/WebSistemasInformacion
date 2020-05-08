56063d70c24688b1e0372d6a7dfdf75d33dd8571
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2020-05-08 13:45:32 +0200

    Update introduce.php

--- form/introduce.php
+++ form/introduce.php
@@ -11,32 +11,9 @@
         $bd="sif_tfp";
         echo "Segundo echo OK";
         // Creamos la conexión pasándole el servidor, nombre de user, y clave. Si no se establece conexión muestra un error.
-        //$conexion = mysqli_connect($servername, $username, $password, $bd);
-        $mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
-        if ($mysqli->connect_errno) {
-            echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-        }
+        $conexion = mysqli_connect($servername, $username, $password, $bd);
         echo "Conección a bd correcta";
-        //Facilitamos recogida de datos con variables sencillas
-        $ejercicio='2020';
-        $Beneficio_bruto=$_POST['Beneficio_bruto'];
-        $Impuestos=$_POST['Impuestos'];
-        $Intereses=$_POST['Intereses'];
-        $Despreciacion=$_POST['Despreciacion'];
-        $Gastos_Generales=$_POST['Gastos_Generales'];
-
-        //Insercion de datos
-        $sql = "INSERT INTO DATOS_ACTIVOS (ANO, Beneficio_bruto, Impuestos, Intereses, Despreciacion, Gastos_Generales)
-        VALUES ('$ejercicio', '$Beneficio_bruto', '$Impuestos', '$Intereses', '$Despreciacion', '$Gastos_Generales')";
-        if (mysqli_query($conexion, $sql)) 
-        {
-          echo "Registro añadido correctamente";
-        } 
-        else 
-        {
-          echo "Error: No se pudieron introducir los datos" . mysqli_error($conexion);
-        }
-        mysqli_close($conexion);
+        
       ?>
    <body>
  </html>
