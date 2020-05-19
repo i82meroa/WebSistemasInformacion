33da74bed5643c9be7349827d33eaf76a8f09b58
David Castejon <diyeicom@gmail.com>
2020-05-19 22:13:03 +0200

    testing37

--- form/dashboardTFP/script.php
+++ form/dashboardTFP/script.php
@@ -245,7 +245,7 @@ let espPopChart1 = new Chart(myChart1, {
   },
   options: {
     title: {
-      display: true,
+      display: false,
       text: 'Beneficio Neto (en euros)',
       fontSize:15
     },
@@ -648,23 +648,14 @@ let espPopChart8 = new Chart(myChart8, {
     datasets: [
       {
         label: "ROA",
-        backgroundColor:[
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 99, 132, 1)'
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)'
         ],
         data: [
           roa[0],
