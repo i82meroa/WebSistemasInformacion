99ad91f3cae55f66c31bd35fb2fdbf76ea2219bb
David Castejon <diyeicom@gmail.com>
2020-06-01 18:32:24 +0200

    cosas

--- form/dashboardTFP/index.php
+++ form/dashboardTFP/index.php
@@ -85,7 +85,7 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal" formaction="../index.html">Salir</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal" formaction="./index.html">Salir</button>
           </div>
         </div>
       </div>
